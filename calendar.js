aff80a1a462d8151cd856d1e704d1b122d4851e7
nhuanthi <tlam1288@gmail.com>
2020-11-10 13:56:34 -0800

    linked calendar date choices to fastSensorAPI call

---
+++ calendar.js
@@ -0,0 +1,15 @@
+//calendar
+let beginDate;
+let endDate;
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'left'
+    }, function(start, end, label) {
+        beginDate = start.format('YYYY-MM-DD');
+        endDate =  end.format('YYYY-MM-DD');
+        console.log(beginDate + " " + endDate);
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+      fastSensorAPI ();
+    });
+  });
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+//calendar
+let beginDate;
+let endDate;
+
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+      opens: 'left'
+    }, function(start, end, label) {
+        beginDate = start.format('YYYY-MM-DD');
+        endDate =  end.format('YYYY-MM-DD');
+        console.log(beginDate + " " + endDate);
+      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD')); 
+      fastSensorAPI();
+    })  
+  });
+
+
 //API
 
-var tokenID = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2MDUwMzQ3NjgsImp0aSI6IllHc0VJN3hSNHVXNU5NUUFETCtGazAyd0hXemFnRFVMTWpCQlJqVTJOa1k9IiwiaXNzIjoiYXBpLmZhc3RzZW5zb3IuY29tIiwibmJmIjoxNjA1MDM0NzY4LCJleHAiOjE2MDUwNDI1NjgsImRhdGEiOnsiY2xpZW50SWQiOiIyMEFGNTY2RiJ9fQ.givQMHpQaQGBWK6fz3OhcWlctWXRjWBnL6qk2K2R6y1C-xLQi_LXK1t581leS-NkkIdxGFmnmBHguRw3nqsNzQ";
+var tokenID = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2MDUwNDI1OTUsImp0aSI6ImhKUFJtTU9SSTNqb0ZlVm05MkVHZk53SXVaeWlmK1lXTWpCQlJqVTJOa1k9IiwiaXNzIjoiYXBpLmZhc3RzZW5zb3IuY29tIiwibmJmIjoxNjA1MDQyNTk1LCJleHAiOjE2MDUwNTAzOTUsImRhdGEiOnsiY2xpZW50SWQiOiIyMEFGNTY2RiJ9fQ._1gVCDCvCe45LgmR1GHu3bAILl2Qwor1JTIaDe6icu1KsnSI1RaVESHJiJjnwvBuwgosH1-qcUz93OFpJJVY0g";
 var locationID = "204D5F25";
 
 $.ajaxSetup({
@@ -8,82 +25,47 @@ $.ajaxSetup({
         xhr.setRequestHeader('Authorization', 'Bearer ' + tokenID);
     }
 });
+
+
+let days = [];
+
+function fastSensorAPI(){
 $.getJSON(
 "https://appsrv.fastsensor.us:8890/v1/locations/" + locationID + "/alerts",
 {
-  start_date: "2020-05-01",
-  end_date: "2020-05-02",
+  start_date: beginDate,
+  end_date: endDate,
   selector: "[ADAM]"
   
   
 } )
 .done(function(data) {
+
+    if(data.data.length === 0){
+        alert("no data in range, please choose other dates");
+        return;
+    }
+
+    console.log(data);
     let newData = Object.entries(data)
     let unixTimestamp = newData[2][1][0].timestamp;
     let date = new Date(unixTimestamp*1000);
 
   console.log("Success:");
+  console.log(newData);
   console.log(date);
+  console.log(newData[2][1]);
   console.log(newData[2][1][0].timestamp); //gets the timestamp from the data object
 })
 .fail(function( jqxhr, textStatus, error ) {
   var err = textStatus + ", " + error;
   console.log( "Request Failed: " + err );
 });
+}
 
 
 
 
 
-//calendar
-$(function() {
-    $('input[name="daterange"]').daterangepicker({
-      opens: 'left'
-    }, function(start, end, label) {
-      console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-    });
-  });
 
 
-// chart
-var ctx = document.getElementById('myChart');
-var ctx = document.getElementById('myChart').getContext('2d');
-var ctx = $('#myChart');
-var ctx = 'myChart';
-
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
-});
