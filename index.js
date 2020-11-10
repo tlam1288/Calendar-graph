53b0547bd726e8125e52e73dc9983a21873ecd81
nhuanthi <tlam1288@gmail.com>
2020-11-10 14:46:57 -0800

    linked x-axis of graph to days array

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ $.getJSON(
 //   console.log(month);
 //   console.log(day);
    console.log(days);
+   graph();
 })
 .fail(function( jqxhr, textStatus, error ) {
   var err = textStatus + ", " + error;
@@ -80,8 +81,50 @@ $.getJSON(
 }
 
 
+// graph
+function graph(){
+var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = $('#myChart');
+var ctx = 'myChart';
 
-
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+      labels: days,
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
+}
 
 
 
